6d7d77339cb8c6f386d92f9073109a109fe12ceb
Nunder <nathannunderwood@gmail.com>
2022-08-03 15:50:18 -0700

    Figure out articleHour number

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,2 +1,33 @@
+var now = moment();
 var today = moment();
+var articleEl = $('article');
+
 $("#currentDay").text(today.format("MMM Do, YYYY"));
+
+for (i=0; i <= articleEl.length; i++) {
+    var dataTime = articleEl[i];
+    var articleHour = parseInt($(dataTime).attr('data-time'));
+    
+    var currentHour = parseInt(moment().format("H"));
+    
+    if (articleHour < currentHour) {
+        $(articleEl).addClass('past');
+    } else if (articleHour === currentHour) {
+        $(articleEl).addClass('present');
+    } else if (articleHour > currentHour) {
+        $(articleEl).addClass('future');
+    }
+}
+console.log(currentHour);
+var articleHour = parseInt($(dataTime).attr('data-time'));
+console.log(articleHour);
+
+
+
+
+
+
+
+
+
+
