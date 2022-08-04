980dff2e68eec1f30f1a70b82e9f675cfe69b1fc
Nunder <nathannunderwood@gmail.com>
2022-08-04 11:02:35 -0700

    Shifting to using textarea for timeblocks instead of articles. Need to build logic to store user input that reloads on refresh

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,12 @@
 var now = moment();
 var today = moment();
-var articleEl = $('article');
+var textAreaEl = $('textarea');
 var blockContainer = $(".block-container");
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 var showNine = document.querySelector('#nine-container');
 
-for (i=0; i <= articleEl.length; i++) {
-    var dataTime = articleEl[i];
+for (i=0; i <= textAreaEl.length; i++) {
+    var dataTime = textAreaEl[i];
     var articleHour = parseInt($(dataTime).attr('data-time'));
     
     var currentHour = parseInt(moment().format("H"));
@@ -20,33 +20,6 @@ for (i=0; i <= articleEl.length; i++) {
     }
 }
 
-// user clicks timeslot to add appointment
-function makeAppointment() {
-    var addButton = document.querySelector('#add-button');
-    addButton.addEventListener('click', function() {
-    showNine.style.display = 'block';
-    addButton.style.display = 'none';
-})
-addAppointment();
-    
-}
-
-function addAppointment() {
-    var saveButton = document.querySelector('#nine-button');
-    saveButton.addEventListener('click', function() {
-        var appointment = document.querySelector('#appointment-name').value;
-        localStorage.setItem('appointment-name', appointment);
-        var setNineAppointment = document.querySelector('#set-nine-appointment');
-        setNineAppointment.textContent = localStorage.getItem('appointment-name');
-        showNine.style.display = 'none';
-
-    })
-}
-
-makeAppointment();
-
-
-// appointment details includes name and...?
 
 
 
