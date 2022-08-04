95edffcfc48afaeab4ce186eb74b02cb15c049a3
Nunder <nathannunderwood@gmail.com>
2022-08-04 15:49:39 -0700

    Add comments to JS. Consolidate code in CSS and JS. Start README content

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,6 @@
-var now = moment();
 var today = moment();
 var textAreaEl = $('textarea');
-var blockContainer = $(".block-container");
-$("#currentDay").text(today.format("MMM Do, YYYY"));
+var sectionEl = $('section');
 var showNine = document.querySelector('#nine-container');
 var saveNine = $('#save-nine');
 var saveTen = $('#save-ten');
@@ -13,7 +11,13 @@ var saveTwo = $('#save-two');
 var saveThree = $('#save-three');
 var saveFour = $('#save-four');
 var saveFive = $('#save-five');
+var clearButton = $('#clear-button');
 
+// current day dynamically updated at top of page
+$("#currentDay").text(today.format("MMM Do, YYYY"));
+
+
+// compare current time to timeblock value to determine text area background color
 for (i=0; i <= textAreaEl.length; i++) {
     var dataTime = textAreaEl[i];
     var articleHour = parseInt($(dataTime).attr('data-time'));
@@ -29,10 +33,12 @@ for (i=0; i <= textAreaEl.length; i++) {
     }
 }
 
+// event listeners for save buttons to store user input in local storage
 saveNine.on('click', function() {
-        if (textAreaEl.val() === null) {
+        if (textAreaEl.val() === "") {
             alert ('please enter a valid appointment');
         } else {
+            alert('Appointment Saved!');
             localStorage.setItem('9am', textAreaEl.val());
         }
     }
@@ -40,9 +46,10 @@ saveNine.on('click', function() {
 
 saveTen.on('click', function() {
     var textTen = $('#text-ten');
-    if (textTen.val() === null) {
+    if (textTen.val() === "") {
         alert ('please enter a valid appointment');
     } else {
+        alert('Appointment Saved!');
         localStorage.setItem('10am', textTen.val());
     }
 }
@@ -50,9 +57,10 @@ saveTen.on('click', function() {
 
 saveEleven.on('click', function() {
     var textEleven = $('#text-eleven');
-    if (textEleven.val() === null) {
+    if (textEleven.val() === "") {
         alert ('please enter a valid appointment');
     } else {
+        alert('Appointment Saved!');
         localStorage.setItem('11am', textEleven.val());
     }
 }
@@ -60,9 +68,10 @@ saveEleven.on('click', function() {
 
 saveTwelve.on('click', function() {
     var textTwelve = $('#text-twelve');
-    if (textTwelve.val() === null) {
+    if (textTwelve.val() === "") {
         alert ('please enter a valid appointment');
     } else {
+        alert('Appointment Saved!');
         localStorage.setItem('12pm', textTwelve.val());
     }
 }
@@ -70,9 +79,10 @@ saveTwelve.on('click', function() {
 
 saveOne.on('click', function() {
     var textOne = $('#text-one');
-    if (textOne.val() === null) {
+    if (textOne.val() === "") {
         alert ('please enter a valid appointment');
     } else {
+        alert('Appointment Saved!');
         localStorage.setItem('1pm', textOne.val());
     }
 }
@@ -80,9 +90,10 @@ saveOne.on('click', function() {
 
 saveTwo.on('click', function() {
     var textTwo = $('#text-two');
-    if (textTwo.val() === null) {
+    if (textTwo.val() === "") {
         alert ('please enter a valid appointment');
     } else {
+        alert('Appointment Saved!');
         localStorage.setItem('2pm', textTwo.val());
     }
 }
@@ -90,9 +101,10 @@ saveTwo.on('click', function() {
 
 saveThree.on('click', function() {
     var textThree = $('#text-three');
-    if (textThree.val() === null) {
+    if (textThree.val() === "") {
         alert ('please enter a valid appointment');
     } else {
+        alert('Appointment Saved!');
         localStorage.setItem('3pm', textThree.val());
     }
 }
@@ -100,9 +112,10 @@ saveThree.on('click', function() {
 
 saveFour.on('click', function() {
     var textFour = $('#text-four');
-    if (textFour.val() === null) {
+    if (textFour.val() === "") {
         alert ('please enter a valid appointment');
     } else {
+        alert('Appointment Saved!');
         localStorage.setItem('4pm', textFour.val());
     }
 }
@@ -110,13 +123,42 @@ saveFour.on('click', function() {
 
 saveFive.on('click', function() {
     var textFive = $('#text-five');
-    if (textFive.val() === null) {
+    if (textFive.val() === "") {
         alert ('please enter a valid appointment');
     } else {
+        alert('Appointment Saved!');
         localStorage.setItem('5pm', textFive.val());
     }
 }
 )
+
+// text areas populated with user input from local storage on refresh
+$('#text-nine').val(localStorage.getItem('9am'));
+$('#text-ten').val(localStorage.getItem('10am'));
+$('#text-eleven').val(localStorage.getItem('11am'));
+$('#text-twelve').val(localStorage.getItem('12pm'));
+$('#text-one').val(localStorage.getItem('1pm'));
+$('#text-two').val(localStorage.getItem('2pm'));
+$('#text-three').val(localStorage.getItem('3pm'));
+$('#text-four').val(localStorage.getItem('4pm'));
+$('#text-five').val(localStorage.getItem('5pm'));
+
+// if user wants to clear all calendar data
+$('#clear-button').on('click', function () {
+    var confirmClear = confirm('Are you sure you want to clear your calendar?')
+
+    if(confirmClear === false) {
+        alert('Phew! That was close');
+    } else {
+        alert('Calendar cleared. Refresh the page');
+        localStorage.clear();
+    }
+})
+
+
+
+
+
 
 
 
---
+++ README.md
@@ -0,0 +1,10 @@
+👦 Nathan Underwood - Module 5 Challenge
+
+This Challenge gives me the opportunity to refactor previous code in order to build a smooth daily planner tool.
+
+Building this quiz showcases my ability to use event listeners 👂, timer intervals ⏰, store user information 💾, and more to create a smooth user experience. We've been told in class to "think like a programmer", and I believe the Javascript code in this project shows me starting to take in what that means. The end-user reads top-to-bottom 📗, but a programmer's code should reflect when functionality is firing 🚀 and events are happening. Building code like this makes it easier for the next person to understand what's going on. 
+
+📼 Here is a short video of what the functioning webpage should look like: <img src= "https://github.com/thundernunder/module-4-challenge/blob/main/assets/coding-quiz.gif" alt="Coding Quiz Runthrough">
+
+
+🔗 Link to the live-site: https://thundernunder.github.io/module-4-challenge/
