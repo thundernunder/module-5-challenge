d2c151589c769f0c5e5a2ca9ae40509d66fcb72c
Nunder <nathannunderwood@gmail.com>
2022-08-03 23:18:43 -0700

    Able to add appointment but there has to be more efficient way. Also appointment disappears on refresh

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,9 +2,8 @@ var now = moment();
 var today = moment();
 var articleEl = $('article');
 var blockContainer = $(".block-container");
-
-
 $("#currentDay").text(today.format("MMM Do, YYYY"));
+var showNine = document.querySelector('#nine-container');
 
 for (i=0; i <= articleEl.length; i++) {
     var dataTime = articleEl[i];
@@ -12,15 +11,43 @@ for (i=0; i <= articleEl.length; i++) {
     
     var currentHour = parseInt(moment().format("H"));
     
-    if (articleHour === currentHour) {
-        $(dataTime).addClass('present');
-    } else if (articleHour < currentHour) {
+    if (articleHour < currentHour) {
         $(dataTime).addClass('past');
+    } else if (articleHour === currentHour) {
+        $(dataTime).addClass('present');
     } else {
         $(dataTime).addClass('future');
     }
 }
-console.log(currentHour);
+
+// user clicks timeslot to add appointment
+function makeAppointment() {
+    var addButton = document.querySelector('#add-button');
+    addButton.addEventListener('click', function() {
+    showNine.style.display = 'block';
+    addButton.style.display = 'none';
+})
+addAppointment();
+    
+}
+
+function addAppointment() {
+    var saveButton = document.querySelector('#nine-button');
+    saveButton.addEventListener('click', function() {
+        var appointment = document.querySelector('#appointment-name').value;
+        localStorage.setItem('appointment-name', appointment);
+        var setNineAppointment = document.querySelector('#set-nine-appointment');
+        setNineAppointment.textContent = localStorage.getItem('appointment-name');
+        showNine.style.display = 'none';
+
+    })
+}
+
+makeAppointment();
+
+
+// appointment details includes name and...?
+
 
 
 
